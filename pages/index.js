d6d981e7671d4594cdb02975721174d12b25973b
Luiz Souza <lsillva.dev@gmail.com>
2022-11-09 17:20:26 -0300

    Adição da funcionalidade de troca de tema

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import config from "../config.json";
 import styled from "styled-components";
 
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 import React from "react";
@@ -11,7 +10,6 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
       <div
         style={{
           display: "flex",
@@ -36,6 +34,7 @@ function HomePage() {
 export default HomePage;
 
 const StyledHeader = styled.div`
+  background-color: ${(theme) => theme.backgroundLevel1};
   img {
     width: 80px;
     height: 80px;
